37ced66622ee1379b47529e062d2e61d939530d6
Simon Myrvig <simo008s@mail.hansenberg.dk>
2023-11-02 18:50:27 +0100

    idk man

--- Program.cs
+++ Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Void Board (){
+    for (int file = 0; file <8; file ++){
+        for (int rank =0; rank <8; rank ++) {
+            bool isLightSquare = (file + rank) % 2 !=0;
+
+            var squareColour = (isLightSquare) ? lightCol : darkCol;
+            var position = new Vector2 (-3.5f + file, -3.5f + rank);
+        } 
+    }
+}
